f914371a88617e7b2600f686537b4b1c1394e2b0
Thor Galle <thorgalle@gmail.com>
2022-05-15 19:19:50 +0300

    Initial commit: working POC

---
+++ background.js
@@ -0,0 +1,54 @@
+
+// Register handler for when the Readup extension Icon gets clicked.
+//
+// Action taken: open the Readup POC page with the requested URL as a query param
+// in the current tab.
+chrome.action.onClicked.addListener((tab) => {
+    chrome.tabs.update({
+        url: `https://web.thorgalle.me/readup-poc/?url=${encodeURIComponent(tab.url)}`
+    })
+    // Note: doing this and accessing the URL requires the "tabs" permission
+    // https://developer.chrome.com/docs/extensions/reference/tabs/#type-Tab
+});
+
+// Allowed origins for web page <-> extension communication.
+const allowedOriginRegex = /https:\/\/web\.thorgalle\.me\/readup-poc\//
+
+// Register handler for messages from the Readup POC page
+//
+// Action taken: send back HTML text (requested with cookies) for the requested URL
+chrome.runtime.onMessageExternal.addListener(
+    async function (request, sender, sendResponse) {
+        console.log(`Received request from ${sender.url} `);
+        if (!allowedOriginRegex.test(sender.url)) {
+            console.warn(`${sender.url} not allowed to request data.`)
+            return;  // don't allow this web page access
+        }
+        if (request.getHTMLFor) {
+            const htmlString = await getArticleHTMLForURL(request.getHTMLFor);
+            console.log(htmlString.substring(1, 300))
+            sendResponse(htmlString);
+        }
+        // to indicate that sendResponse will be called async, 
+        // return true or a promise
+        return true;
+    }
+);
+
+async function getArticleHTMLForURL(url) {
+    console.log("Fetching URL: ", url);
+    return fetch(url, {
+        credentials: "include",
+        method: "GET",
+        mode: "cors",
+    })
+        .catch(e => console.error("Requesting article page failed ", e))
+        .then(r => r.text())
+        .catch(e => console.error("HTTP response parsing failed ", e))
+    // .then(t => console.log(t.substring(0, 300)))
+    // .then(t => {
+    //     const paragraph = /<p>(.*Twee weken.*)<\/p>/.exec(t);
+    //     console.log(paragraph[0]);
+    // });
+}
+
